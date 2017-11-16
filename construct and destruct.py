90f853dfbfcb620f0fb0ee37717367e297cb5641
Saurav <saurav.kec@gmail.com>
2017-11-16 12:44:53 +0530

    constructor and destructor example

---
+++ construct and destruct.py
@@ -0,0 +1,29 @@
+class Human:
+    """Represents Human being"""
+    population = 0
+
+    def __init__(self, name):
+        self.name = name
+        Human.population += 1
+
+    def __del__(self):
+        Human.population = Human.population - 1
+        print("Decrementing the population")
+
+    def sayHi(self):
+        print("Hi, My name is %s" % self.name)
+
+    def howMany(self):
+        if Human.population == 1:
+            print("I am the only one here")
+
+        else:
+            print("there are %d guys left" % Human.population)
+
+per1 = Human("Rohan")
+per1.sayHi()
+per1.howMany()
+
+per2 = Human("Rajesh")
+per2.howMany()
+
